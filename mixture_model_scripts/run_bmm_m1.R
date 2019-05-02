b280b50f6f9d14d4d40c3dc71fcfcc32d9f5b4fa
erflynn <erflynn@stanford.edu>
2019-05-02 16:06:07 -0700

    fixed filtering bugs

--- mixture_model_scripts/run_bmm_m1.R
+++ mixture_model_scripts/run_bmm_m1.R
@@ -11,7 +11,9 @@
 
 source('model_utils.R')
 source('heritability_utils.R')
-#source('model_utils_v3.R')
+require('R.utils')
+require('data.table')
+
 DATA.FOLDER <- "/scratch/PI/mrivas/users/erflynn/sex_div_gwas/data/"
 
 
@@ -60,60 +62,49 @@ print(sprintf("Running M1 for trait %s with %s dim, %s, with prefixes %s", trait
 #print(sprintf("Running M1 for trait %s with MAF cutoff and SE cutoff %s.", trait, maf.cutoff, se.cutoff))
 
 #snps.to.keep <- filterMAF(maf.cutoff)
+biomarker = FALSE
 
+loadBiomarkerDat <- function(trait, sex){
+  dir <- "/oak/stanford/groups/mrivas/projects/biomarkers/results/plink/combined"
+  file.path <- sprintf("%s/%s_%s.glm.linear.gz", dir, trait, sex)
+  dat <- fread(file.path, data.table=FALSE)
+
+   # select only the rows with the additive model
+    dat.1 <- dat[dat$TEST == "ADD",]
+    rownames(dat.1) <- dat.1$ID
+
+    # remove NAs
+    dat.2 <- dat.1[!is.na(dat.1$SE),]
+
+    # SE filter
+    dat.3 <- dat.2[dat.2$SE < QUANT.SE.CUTOFF,]
+
+    # MAF filter
+    dat.4 <- dat.3[dat.3$ID %in% snps.to.keep$V1,]
+    colnames(dat.4)[1:3] <- c("CHR", "BP", "SNP")
+
+    return(dat.4)
+}
 
 loadDat <- function(trait, trait.type){
     
+
+    if (biomarker==TRUE){
+     list.ds <- lapply(c("female", "male"), function(sex) loadBiomarkerDat(trait, sex))
+    } else {
     # for each trait
     list.ds <- lapply(list.prefixes, function(prefix) {
         all.dat <- do.call(rbind, lapply(chrs, function(chr) { getFile(prefix, chr, trait)}));
         colnames(all.dat)[1:3] <- c("CHR", "BP", "SNP");
         return(all.dat)
     })
+    }
 
     list.ds2 <- extractOverlappingRows(list.ds)
 
     stan.obj <- extractDataStanMulti(list.ds2)
     return(stan.obj)
 }
-
-# loadDat <- function(trait, trait.type){
-
-
-#	# function for loading the data
-
-#	# load all the data
-#	if (trait.type == 'binary'){
-#		all.dat <- lapply(chrs, function(x){ getDataBin(as.character(x), trait)})
-#	} 
-#	if (trait.type == 'quant') {
-#		all.dat <- lapply(chrs, function(x){ getDataQuant(as.character(x), trait)})
-#	}
-
-#	# reformat data, remove rows that are not shared
-#     dat.reform <- reformatData(all.dat, trait.type, maf.cutoff)
-#     filt.f <- dat.reform$`1`
-#     filt.m <- dat.reform$`2`
-
-#     # filter by standard error
-#     dat.filt <- filterSE(filt.f, filt.m, trait.type, cutoff=se.cutoff)
-#     filt.f <- dat.filt$`1`
-#     filt.m <- dat.filt$`2`
-
-
-#     # extract dat in a format for stan input
-#     dat <- extractDataStan(filt.f, filt.m)
-
-#     return(dat)
-# }
-
-# loadDat3 <- function(trait, trait.type){
-
-#      all.dat <- lapply(c(chrs), function(x){ getDataQuantMeno(as.character(x), trait, trait.type)})
-#      reform.dat <- reformDat3(all.dat, "quant")    
-#      dat <- extractDataStan3(reform.dat)
-#      return(dat)
-# }
 
 
 runM1 <- function(trait, trait.type){
@@ -122,7 +113,7 @@ runM1 <- function(trait, trait.type){
     dat <- loadDat(trait, trait.type)
     print("Data loaded")
     dat$dat$K <- 2
-    save(dat, file=sprintf("%s/age_sex_meno/dat_%s.RData", DATA.FOLDER, trait))
+    save(dat, file=sprintf("%s/biomarker/dat_%s.RData", DATA.FOLDER, trait))
     print("LEARNING PARAMS")
     fit1 <- stan(file = "models/model1_no_loglik.stan",  
             data = dat$dat,    
@@ -130,7 +121,7 @@ runM1 <- function(trait, trait.type){
     print("SAVING")
     rm(dat)
     print(fit1, pars=c("Sigma", "pi", "Omegacor"), probs=c(0.025, 0.5, 0.975), digits_summary=5)
-    save(fit1, file=sprintf("%s/age_sex_meno/f_%s.RData", DATA.FOLDER, trait))
+    save(fit1, file=sprintf("%s/biomarker/f_%s.RData", DATA.FOLDER, trait))
 
 }
 
@@ -138,8 +129,8 @@ extractData <- function(trait){
 
     print("EXTRACTING")
     # load the data and fit to extract info about the run
-    load(file=sprintf("%s/age_sex_meno/dat_%s.RData", DATA.FOLDER, trait))
-    load(file=sprintf("%s/age_sex_meno/f_%s.RData", DATA.FOLDER, trait))
+    load(file=sprintf("%s/biomarker/dat_%s.RData", DATA.FOLDER, trait))
+    load(file=sprintf("%s/biomarker/f_%s.RData", DATA.FOLDER, trait))
     
     m1.pi <- getPi(fit1)
     m1.Sigma <- getSigmaMulti(fit1, ndim)
@@ -168,7 +159,7 @@ extractData <- function(trait){
 
     # TODO - label these better
 
-    write.table(next.row, file=sprintf("%s/summary_dat_%s_%s_%s.txt", DATA.FOLDER, trait, ndim, downsampled_str), row.names=FALSE, quote=FALSE)
+    write.table(next.row, file=sprintf("%s/biomarker/summary_dat_%s_%s_%s.txt", DATA.FOLDER, trait, ndim, downsampled_str), row.names=FALSE, quote=FALSE)
 }
 
 runM1(trait, trait.type)
