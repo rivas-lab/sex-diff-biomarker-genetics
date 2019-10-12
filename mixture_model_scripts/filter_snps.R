3de0aa29ba96dab93b506b8caa6f0f4e97ce41db
erflynn <erflynn@stanford.edu>
2019-10-12 15:26:01 -0700

    added figure notebooks

--- mixture_model_scripts/filter_snps.R
+++ mixture_model_scripts/filter_snps.R
@@ -19,6 +19,11 @@ rem.snps <- filt.file[filt.file$all_filters==0,] # 655,654 out of 784,256
 # filter for ld - part of LD-pruned set
 rem.snps2 <- rem.snps[rem.snps$ld_indep,] # 361,424
 
+# updated LD filtering later
+prune_snps <- read.table("/scratch/PI/mrivas/users/erflynn/sex_div_gwas/gwas_scripts/ld_dat/all_prune.in", header=FALSE, colClasses="character")
+keep.snps <- (intersect(rem.snps$ID,prune_snps$V1))
+
 write.table(rem.snps2, sprintf("%s/snp_filt_metadata.txt", OUT_DIR))
 
 write.table(rem.snps2$ID, sprintf("%s/snp_filt_list.txt", OUT_DIR), col.names=FALSE, row.names=FALSE, quote=FALSE)
+write.table(keep.snps, sprintf("%s/snp_filt_list2.txt", OUT_DIR), col.names=FALSE, row.names=FALSE, quote=FALSE)
